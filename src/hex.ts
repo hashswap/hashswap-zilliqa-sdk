4ac672e94775bae6debeb3ee36a6c3a8748fd27c
deovrat2005 <deovrat2005@gmail.com>
2021-08-18 14:58:18 +0200

    1.0.135

--- src/hex.ts
+++ src/hex.ts
@@ -1536,9 +1536,8 @@ export class Hex {
     // Get Sponsortoken
     const sponsors: { [key in string]: SponsorToken } = {}
     poolTokenHashes.forEach(tokenHash => {
-      
       if (!contractState.sponsors[tokenHash]) return
-      
+
       const poolSponsors = contractState.sponsors[tokenHash]
       const userSponserToken = poolSponsors![currentUser!]
       console.log('888888888888888888888')
@@ -1577,7 +1576,7 @@ export class Hex {
     const zerosponsor: { [key in string]: SponsorToken } = {}
     poolTokenHashes.forEach(tokenHash => {
       if (!contractState.zero_sponsor[tokenHash]) return
-      
+
       const poolZeroSponsors = contractState.zero_sponsor[tokenHash]
       const zeroSponserToken = poolZeroSponsors![ZERO_HASH]
       console.log('888888888888888888888')
